154bcac11bdf7bcd112b1c3c0462e9abd98be58d
pesto131 <ricola1304@gmail.com>
2023-01-30 07:13:29 +0100

    aggiunto testo formattato

--- src/mioTelegram.py
+++ src/mioTelegram.py
@@ -8,11 +8,16 @@ bot = telebot.TeleBot(TOKEN)
 
 def inviaMessaggio(studente):
     text = ""
-    text += studente.materia
+    text += f"<b>{studente.materia}</b>"
     text += "\n"
     text += studente.info
+    text += "\n"
+    text += "\n"
+    text += studente.lezioni
+    text += "\n"
+    #text += f"<a target='_blank' rel='noreferrer' href='https://www.google.com/url?q=https://api.whatsapp.com/send?phone%3D390230566620%26text%3D1327282322&amp;sa=D&amp;source=editors&amp;ust=1675061479155087&amp;usg=AOvVaw0I97OsdXFBS449J9eA6J7g'>Riferimento: {studente.numero}</a>"
     
-    bot.send_message(-873790098, text)
+    bot.send_message(chat_id=-873790098, text=text, parse_mode="html")
 
 
 #bot.infinity_polling()
