6b40738e4f98372cfdc7cedd8348cb5b59bae22d
pesto13 <ricola1304@gmail.com>
2023-01-31 04:55:24 +0100

    moreMaterieFunzionante

--- src/main.py
+++ src/main.py
@@ -6,12 +6,18 @@ import mioTelegram
 from ischedule import schedule, run_loop
 
 
+URLS = ["https://docs.google.com/spreadsheets/d/e/2PACX-1vSxa-l5fqDBv5hOuNQ5q0kW19YOpPHAMe-kITWnBR567PIBhYkklOzpbjz-td77hY-jBw5_KGyz1fX7/pubhtml?gid=6878166&&range=A1:H&widget=false&chrome=false&headers=false&",
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vSxa-l5fqDBv5hOuNQ5q0kW19YOpPHAMe-kITWnBR567PIBhYkklOzpbjz-td77hY-jBw5_KGyz1fX7/pubhtml?gid=695965261&range=A1:H&widget=false&chrome=false&headers=false&"]
+
+IDS = ["6878166",
+       "695965261"]
+
 codici = []
 
 #ogni volta devo svuotarla immagino
 def riempiLista(s):
     index = 0
-    limit = 20
+    limit = 30
     studenti=[]
     for riga in s:
 
@@ -34,37 +40,37 @@ def riempiLista(s):
         if index >= limit: #piccola cosa quasi inutile per ridurre
             return studenti
 
-def leggiVecchiCodici():
-    with open('codici.txt', 'r', encoding="utf-8") as f:
+def leggiVecchiCodici(i):
+    with open(f'codici{i}.txt', 'r', encoding="utf-8") as f:
         return f.read()
 
-def scriviCodici():
-    with open('codici.txt', 'w', encoding="utf-8") as f:
+def scriviCodici(i):
+    with open(f'codici{i}.txt', 'w', encoding="utf-8") as f:
         for c in codici:
             f.write(f"{c} ")
 
 def mergeCodici(vecchi, studenti):
     for s in studenti:
         if s.numero not in vecchi:
-            pass
-            #mioTelegram.inviaMessaggio(s)
+            mioTelegram.inviaMessaggio(s)
 
 def main():
     print("prima riga")
-    URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSxa-l5fqDBv5hOuNQ5q0kW19YOpPHAMe-kITWnBR567PIBhYkklOzpbjz-td77hY-jBw5_KGyz1fX7/pubhtml?gid=6878166&&range=A1:H&widget=false&chrome=false&headers=false&"
-    r = requests.get(URL)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    print("prima riga")
-    mioTelegram.invio("inizio ricerca")
-    print("prima riga")
-    s = soup.find("div", {"id":"6878166"}) #file di informatica, potrebbe cambiare nel tempo?
-    s = s.contents[0].contents[0].contents[1]  #questo è il tbody
 
-    vecchiCodici = leggiVecchiCodici()
-    studenti = riempiLista(s)
-    print(studenti[0])
-    mergeCodici(vecchiCodici, studenti)
-    scriviCodici()
+    for i in range(2):
+        r = requests.get(URLS[i])
+        soup = BeautifulSoup(r.content, 'html.parser')
+        print("prima riga")
+        #mioTelegram.invio("inizio ricerca")
+        print("prima riga")
+        s = soup.find("div", {"id":IDS[i]}) #file di informatica, potrebbe cambiare nel tempo?
+        s = s.contents[0].contents[0].contents[1]  #questo è il tbody
+
+        vecchiCodici = leggiVecchiCodici(i)
+        studenti = riempiLista(s)
+        print(studenti[0])
+        mergeCodici(vecchiCodici, studenti)
+        scriviCodici(i)
 
 
 if __name__ == "__main__":
